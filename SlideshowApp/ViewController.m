b454ea9900a8e50f50db72289b2e35e47ecd6f5f
橋本 遥 <hashimotoharuka@hashimoto-no-imac-3.local>
2016-01-23 01:59:14 +0900

    前回提出したものが白紙に近い状態だったようです。再度アップいたします。

--- SlideshowApp/ViewController.m
+++ SlideshowApp/ViewController.m
@@ -8,16 +8,83 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController (){
+    UIImageView *aImageView;
+    NSInteger countNumber;
+}
+
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+-(void)viewDidLoad{
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    countNumber = 1;
+    [self setupBackground];
+    [self setupButton];
+
+
 }
+
+   // 背景画像の表示
+-(void)setupBackground{
+    aImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 320, 568)];
+    aImageView.image = [UIImage imageNamed:@"1.png"];
+    [self.view addSubview:aImageView];
+}
+
+-(void)setupButton{
+// サイズ・位置を指定してボタンを作る（※バックボタン）
+UIButton *aButton = [UIButton buttonWithType:UIButtonTypeCustom];
+aButton.frame = CGRectMake(0, 0, 90, 90);
+aButton.center = CGPointMake(80, 430);
+[aButton setImage:[UIImage imageNamed:@"b1.png"] forState:UIControlStateNormal];
+[aButton setImage:[UIImage imageNamed:@"b2.png"] forState:UIControlStateHighlighted];
+// ボタンで呼び出すメソッドとその操作を指定
+[aButton addTarget:self action:@selector(changeImageUsingnIfB:) forControlEvents:UIControlEventTouchUpInside];
+// 画面にボタンを貼り付ける
+[self.view addSubview:aButton];
+
+
+// サイズ・位置を指定してボタンを作る（※ネクストボタン）
+UIButton *bButton = [UIButton buttonWithType:UIButtonTypeCustom];
+bButton.frame = CGRectMake(0, 0, 90, 90);
+bButton.center = CGPointMake(250, 430);
+[bButton setImage:[UIImage imageNamed:@"n1.png"] forState:UIControlStateNormal];
+[bButton setImage:[UIImage imageNamed:@"n2.png"] forState:UIControlStateHighlighted];
+// ボタンで呼び出すメソッドとその操作を指定
+[bButton addTarget:self action:@selector(changeImageUsingnIfN:) forControlEvents:UIControlEventTouchUpInside];
+// 画面にボタンを貼り付ける
+[self.view addSubview:bButton];
+}
+
+
+// バックボタン用の命令
+-(void)changeImageUsingnIfB:(id)sender{
+    countNumber--;
+    if (countNumber==0) {
+        aImageView.image = [UIImage imageNamed:@"3.png"];
+    } else if (countNumber==-1) {
+        aImageView.image = [UIImage imageNamed:@"2.png"];
+    } else if (countNumber==-2) {
+        aImageView.image = [UIImage imageNamed:@"1.png"];
+        countNumber = 1;
+    }
+}
+
+
+// ネクストボタン用の命令
+-(void)changeImageUsingnIfN:(id)sender{
+    countNumber++;
+    aImageView.image = [UIImage imageNamed:[NSString stringWithFormat:@"%ld.png",(long)countNumber]];
+    if (countNumber==3) {
+    countNumber = 0;
+    }
+}
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
